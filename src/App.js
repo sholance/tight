62617b18402d2a9f9e36363e4c983bd80daf43be
Sholance <ohksamie@gmail.com>
2020-10-22 03:38:52 +0100

    added chat.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
 import "./App.css";
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
-      {/* Header */}
-      {/* SideBar */}
-      {/* Chat screen */}
-      </div>
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome Here</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
