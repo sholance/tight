689a9d57cbb6e9426fd4d1349a8627cbbaa56ae8
Sholance <ohksamie@gmail.com>
2020-10-22 02:23:05 +0100

    installed firebase

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Sidebar.css";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
@@ -13,7 +13,17 @@ import AppsIcon from "@material-ui/icons/Apps";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
+import db from "../firebase";
+
 function Sidebar() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    db.collection('rooms').onSnapshot(snapshot => {
+      setChannels()
+    })
+  }, [])
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhoSw92isGSWXpfLnRsZO3kSdCnETehPw",
+    authDomain: "tight-shola.firebaseapp.com",
+    databaseURL: "https://tight-shola.firebaseio.com",
+    projectId: "tight-shola",
+    storageBucket: "tight-shola.appspot.com",
+    messagingSenderId: "973193815525",
+    appId: "1:973193815525:web:8e6efdccbe4937f3b0bb0c",
+    measurementId: "G-JVHN83KPFW"
+  };
+
+  const firebaseApp  = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider};
+  export default db;
+
+  
